a1ba3bfa88e216d430df52f922ce6c054c328dcc
Daniel Czerwonk <daniel@dan-nrw.de>
2011-09-18 21:11:28 +0200

    Added license info to classes

--- FoundationExtensions/NSArray+Grouping.h
+++ FoundationExtensions/NSArray+Grouping.h
@@ -3,13 +3,26 @@
 //  FoundationExtensions
 //
 //  Created by Daniel Czerwonk on 9/18/11.
-//  Copyright 2011 Mauve Mailorder Software GmbH & Co. KG. All rights reserved.
+//  Copyright 2011 Daniel Czerwonk <d.czerwonk@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSArray (Grouping)
 
-
+- (NSDictionary *)groupUsingKeySelector:(id (^)(id obj))selectorBlock sortedBy:(NSComparator)comparator;
 
 @end
--- FoundationExtensions/NSMutableURLRequest+BasicHTTPAuth.h
+++ FoundationExtensions/NSMutableURLRequest+BasicHTTPAuth.h
@@ -3,13 +3,29 @@
 //  FoundationExtensions
 //
 //  Created by Daniel Czerwonk on 9/18/11.
-//  Copyright 2011 Mauve Mailorder Software GmbH & Co. KG. All rights reserved.
+//  Copyright 2011 Daniel Czerwonk <d.czerwonk@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>
 //
 
 #import <Foundation/Foundation.h>
 
 @interface NSMutableURLRequest (BasicHTTPAuth)
 
+/*
+ * Sets auth info for Basic-HTTP-Authentication in HTTP header.
+ */
 - (void)setBasicHTTPCredentialForUsername:(NSString *)username withPassword:(NSString *)password;
 
 @end
